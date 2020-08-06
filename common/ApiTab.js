38e5c724cc9e586be7096156effff0a95e5a958f
gamerslouis <gamerslouis@gmail.com>
2020-08-06 19:19:37 +0800

    檔案整理
    1. 優化UI
    2. 移除不必要的API權限需求和檔案

--- common/ApiTab.js
+++ common/ApiTab.js
@@ -1,7 +1,7 @@
 export function makeTabFromApiTab(apiTab, args = {}) {
     apiTab.managerSelect = (args.managerSelect != undefined) ? args.managerSelect : false;
     apiTab.matchSearch = (args.matchSearch != undefined) ? args.matchSearch : true;
-    apiTab.labelColor = (args.labelColor != undefined) ? args.labelColor : 'rgb(249, 255, 254)';
+    apiTab.labelColor = (args.labelColor != undefined) ? args.labelColor : 'rgb(230, 240, 240)';
     apiTab.StaticTitle = (args.StaticTitle != undefined) ? args.StaticTitle : null;
     apiTab.tagName = (args.tagName != undefined) ? args.tagName : 'tes';
     apiTab.taged = (args.taged != undefined) ? args.taged : false;
